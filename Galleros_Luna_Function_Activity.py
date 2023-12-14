0aabe732fbc82be23a9d736094577a4c096f0afc
Gwyiomi <145018234+Gwyiomi@users.noreply.github.com>
2023-12-14 22:30:11 +0800

    Update Galleros_Luna_Function_Activity.py

--- Galleros_Luna_Function_Activity.py
+++ Galleros_Luna_Function_Activity.py
@@ -11,20 +11,20 @@ third_number = int(input("Enter 3rd integer number: "))
 
 def add_or_multiply():
     if first_number == second_number == third_number:
-        multiply_numbers = first_number * second_number * third_number
-        print(f"The product of {first_number}, {second_number}, and {third_number} is {multiply_numbers}")
+        result = first_number * second_number * third_number
+        print(f"The product of {first_number}, {second_number}, and {third_number} is {result}")
     elif first_number == second_number:
-        multiply_numbers = first_number * second_number + third_number
-        print(f"The result of {first_number}, {second_number}, and {third_number} is {multiply_numbers}")
+        result = first_number * second_number + third_number
+        print(f"The result of {first_number} multiplied by {second_number} plus {third_number} is {result}")
     elif first_number == third_number:
-        multiply_numbers = (first_number * third_number) + second_number
-        print(f"The product of {first_number}, {second_number}, and {third_number} is {multiply_numbers}")
+        result = (first_number * third_number) + second_number
+        print(f"The result of {first_number} multiplied by {third_number} plus {second_number} is {result}")
     elif second_number == third_number:
-        multiply_numbers = (second_number * third_number) + first_number
-        print(f"The product of {first_number}, {second_number}, and {third_number} is {multiply_numbers}")
+        result = (second_number * third_number) + first_number
+        print(f"The product of {second_number} multiplied by {third_number} plus {first_number} is {result}")
     else:
-        add_numbers = first_number + second_number + third_number
-        print(f"The sum of {first_number}, {second_number}, and {third_number} is {add_numbers}")
+        result = first_number + second_number + third_number
+        print(f"The sum of {first_number}, {second_number}, and {third_number} is {result}")
 
 
 add_or_multiply()
